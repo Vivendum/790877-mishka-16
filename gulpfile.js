9e30119167dcea0ea54611c5951ddc55146972d7
Vivendum <vivendum@mail.ru>
2019-05-13 20:43:37 +0500

    Указывает таски для копирования и удаления файлов в build

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
 "use strict";
 
 var gulp = require("gulp");
+var del = require("del");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+
+gulp.task("clean", function () {
+  return del(["build/**", "!build"]);
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**/*.{svg,jpg,png,webp}",
+    "source/js/**/*.js",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"));
+});
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
